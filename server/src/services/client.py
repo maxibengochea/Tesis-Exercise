226812f01dfda752be5d7b8bdbbd20ba133d9864
human-toilet <maxibengochea67@gmail.com>
2025-01-09 16:02:42 -0500

    add hexadecimal public key

--- server/src/services/client.py
+++ server/src/services/client.py
@@ -8,6 +8,7 @@ class Client:
   def __init__(self, organization_name='', common_name='', country='CU', state='La Habana', locality='Playa', ):
     self._common_name = common_name
     self._private_key = self._create_private_key()
+    self._hex_public_key = self._create_hex_public_key() #llave publica en formato hexadecimal
     self._country = country
     self._state = state
     self._locality = locality
@@ -44,3 +45,19 @@ class Client:
 
     print(f"CSR emited by {self._common_name}")
     return csr_path
+
+  def _create_hex_public_key(self):
+    #extraer la clave pública
+    public_key = self._private_key.public_key()
+
+    #serializar la clave pública en formato hexadecimal para usarla en el enode
+    public_key_bytes = public_key.public_bytes( encoding=serialization.Encoding.DER,
+                                               format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    #convertir a hexadecimal (sin encabezado DER)
+    return public_key_bytes.hex()
+
+  @property
+  def common_name(self): return self._common_name
+  @property
+  def hex_public_key(self): return self ._hex_public_key
