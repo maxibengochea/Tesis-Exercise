c91fb2c3e28e520bd15dfc7879dafa2a71b921fd
human-toilet <maxibengochea67@gmail.com>
2025-01-14 17:28:30 -0500

    add INIT and START to init genesis en nodes and run

--- server/src/assets/start_node_template.py
+++ server/src/assets/start_node_template.py
@@ -1,15 +1,16 @@
-START = '''
-  geth --datadir /data \
-    --networkid 10 \
-    --raft \
-    --raftport 5040{0} \
-    --rpc \
-    --rpcaddr "0.0.0.0" \
-    --rpcport 2200{1} \
-    --rpcapi "admin,eth,debug,miner,net,txpool,personal,web3" \
-    --port 2100{1} \
-    --nodiscover \
-    --tls \
-    --tls.cacert "/tls/root_cert.pem" \
-    --tls.cert "/tls/cert.pem" \
-    --tls.key "/tls/private_key.pem"'''
+START = '''export PRIVATE_CONFIG=ignore
+geth --datadir data 
+    --networkid 1337 --nodiscover --verbosity 5 
+    --syncmode full 
+    --istanbul.blockperiod 5 --mine --miner.threads 1 --miner.gasprice 0 --emitcheckpoints 
+    --http --http.addr {1} --http.port 2200{0} --http.corsdomain "*" --http.vhosts "*" 
+    --ws --ws.addr {1} --ws.port 3200{0} --ws.origins "*" 
+    --http.api admin,eth,debug,miner,net,txpool,personal,web3,istanbul 
+    --ws.api admin,eth,debug,miner,net,txpool,personal,web3,istanbul 
+    --unlock $(grep -o '"address": *"[^"]*"' ./data/keystore/accountKeystore | grep -o '"[^"]*"$' | sed 's/"//g') --allow-insecure-unlock --password ./data/keystore/accountPassword 
+    --port 3030{0}
+    --tlskeyfile "/tls/private_key.pem" 
+    --tlscertfile "/tls/cert.pem" 
+    --tlsca "/tls/root_cert.pem"'''
+
+INIT = '''geth --datadir data init data/data/genesis.json'''
