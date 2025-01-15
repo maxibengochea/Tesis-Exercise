d6b9e20a815668359ce3395e02cd6fb42ab41418
human-toilet <maxibengochea67@gmail.com>
2025-01-15 04:24:48 -0500

    remove generate hex key

--- server/src/services/client.py
+++ server/src/services/client.py
@@ -9,7 +9,6 @@ class Client:
   def __init__(self, organization_name='', common_name='', country='CU', state='La Habana', locality='Playa', ):
     self._common_name = common_name
     self._private_key = self._create_private_key()
-    self._hex_public_key = self._create_hex_public_key() #llave publica en formato hexadecimal
     self._country = country
     self._state = state
     self._locality = locality
@@ -19,9 +18,9 @@ class Client:
     key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
     #guardar la clave privada
-    os.makedirs(f'quorum-network/node{Network.client_number}', exist_ok=True)
+    os.makedirs(f'quorum-network/node{Network.client_number}/tls', exist_ok=True)
 
-    with open(os.path.join(f'quorum-network/node{Network.client_number}', "private_key.pem"), "wb") as f:
+    with open(os.path.join(f'quorum-network/node{Network.client_number}/tls', "private_key.pem"), "wb") as f:
         f.write(key.private_bytes(encoding=serialization.Encoding.PEM, 
                                   format=serialization.PrivateFormat.TraditionalOpenSSL, 
                                   encryption_algorithm=serialization.NoEncryption()))
@@ -46,19 +45,3 @@ class Client:
 
     print(f"CSR emited by {self._common_name}")
     return csr_path
-
-  def _create_hex_public_key(self):
-    #extraer la clave pública
-    public_key = self._private_key.public_key()
-
-    #serializar la clave pública en formato hexadecimal para usarla en el enode
-    public_key_bytes = public_key.public_bytes( encoding=serialization.Encoding.DER,
-                                               format=serialization.PublicFormat.SubjectPublicKeyInfo)
-
-    #convertir a hexadecimal (sin encabezado DER)
-    return public_key_bytes.hex()
-
-  @property
-  def common_name(self): return self._common_name
-  @property
-  def hex_public_key(self): return self ._hex_public_key
