87c5bb9604a3ae11837c5f4f62c00d576269a97f
human-toilet <maxibengochea67@gmail.com>
2025-01-28 05:42:48 -0500

    add genesis template

---
+++ server/src/assets/genesis_template.py
@@ -0,0 +1,46 @@
+GENESIS = '''{
+  "nonce": "0x0",
+  "timestamp": "0x58ee40ba",
+  "extraData": "0xf88fa00000000000000000000000000000000000000000000000000000000000000000f86994f92393af043e2c146ab73aa632c658f727783b2c9436a446957018c3c3ca0984bdef294d88d2ab8228948aac4c95236a9ed807cf3eca5261064577960f8994560c2e4fbd0ac90dbbda4502f189170a41e3601694baf50f31fdd31c95b0722ffeb54bf0ddebb4c270c080c0",
+  "gasLimit": "0xFFFFFF",
+  "gasUsed": "0x0",
+  "number": "0x0",
+  "difficulty": "0x1",
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "mixHash": "0x63746963616c2062797a616e74696e65206661756c7420746f6c6572616e6365",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "config": {
+    "chainId": 1337,
+    "homesteadBlock": 0,
+    "eip150Block": 0,
+    "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "eip155Block": 0,
+    "eip158Block": 0,
+    "byzantiumBlock": 0,
+    "constantinopleBlock": 0,
+    "petersburgBlock": 0,
+    "istanbulBlock": 0,
+    "muirglacierblock": 0,
+    "berlinBlock": 0,
+    "londonBlock": 0,
+    "isQuorum": true,
+    "maxCodeSizeConfig": [
+      {
+        "block": 0,
+        "size": 64
+      }
+    ],
+    "txnSizeLimit": 64,
+    "qbft": {
+      "policy": 0,
+      "epoch": 30000,
+      "ceil2Nby3Block": 0,
+      "testQBFTBlock": 0,
+      "blockperiodseconds": 5,
+      "emptyblockperiodseconds": 60,
+      "requesttimeoutseconds": 10
+    }
+  },
+  "alloc": {
+alloc_content  }
+}'''
